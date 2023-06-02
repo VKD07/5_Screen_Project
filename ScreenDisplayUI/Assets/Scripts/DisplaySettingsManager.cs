b7e5c3e3e96b598dbe1c637fb2c981bd06c8aa23
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-06-02 15:38:57 +0400

    Added more functionalities

--- ScreenDisplayUI/Assets/Scripts/DisplaySettingsManager.cs
+++ ScreenDisplayUI/Assets/Scripts/DisplaySettingsManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,15 +12,20 @@ public class DisplaySettingsManager : MonoBehaviour
     [SerializeField] GameObject m_displayPage;
     [SerializeField] GameObject m_backBtn;
 
-    [Header("ShowDisplay/SelectDisplay_Buttons")]
+    [Header("ShowDisplay/SelectDisplay_Buttons -------------------")]
     [SerializeField] Button m_fullDisplayBtn;
     [SerializeField] Button m_selectDisplayBtn;
 
-    [Header("Display Buttons")]
+    [Header("Display Buttons -------------------")]
     [SerializeField] Button[] m_displaysAvailable;
 
+    [Header("Screen Resolution buttons -------------------")]
+    [SerializeField] Button[] m_btn4by3;
+    [SerializeField] Button m_btn16by9;
+    [SerializeField] Button[] m_fullScreenBtns;
+
     [Header("Screen Displays")]
-    [SerializeField] GameObject[] m_screenDisplays; 
+    [SerializeField] GameObject[] m_screenDisplays;
 
     private void Start()
     {
@@ -29,9 +36,44 @@ public class DisplaySettingsManager : MonoBehaviour
     {
         m_fullDisplayBtn.onClick.AddListener(FullDisplayBtn);
         m_selectDisplayBtn.onClick.AddListener(SelectDisplayBtn);
+
+        m_displaysAvailable[0].onClick.AddListener(() => ShowSetting(0));
+        m_displaysAvailable[1].onClick.AddListener(() => ShowSetting(1));
+        m_displaysAvailable[2].onClick.AddListener(() => ShowSetting(2));
+
+        m_btn4by3[0].onClick.AddListener(() => AdjustResolutionTo4By3(0));
+        m_btn4by3[1].onClick.AddListener(() => AdjustResolutionTo4By3(1));
+
+        m_fullScreenBtns[0].onClick.AddListener(() => SetFullScreenMode(0));
+        m_fullScreenBtns[1].onClick.AddListener(() => SetFullScreenMode(1));
+        m_fullScreenBtns[2].onClick.AddListener(() => SetFullScreenMode(2));
     }
 
-    #region FullDisplay/SelectDisplay
+    void ShowSetting(int btnIndex)
+    {
+        //getting the child object from the btn
+        GameObject btnSettingObj = m_displaysAvailable[btnIndex].gameObject.transform.GetChild(0).gameObject;
+        btnSettingObj.SetActive(!btnSettingObj.activeSelf);
+    }
+
+    void AdjustResolutionTo4By3(int btnIndex)
+    {
+        if(btnIndex == 0)
+        {
+            print("Screen Adjusted by 4:3, Right");
+        }
+        else
+        {
+            print("Screen Adjusted by 4:3, Left");
+        }
+    }
+
+    void SetFullScreenMode(int btnIndex)
+    {
+
+    }
+
+    #region FullDisplay/SelectDisplay_Option
     void FullDisplayBtn()
     {
         ChangeDisplayBtnColors(Color.white);
@@ -44,7 +86,6 @@ public class DisplaySettingsManager : MonoBehaviour
     }
 
     #endregion
-
     void ChangeDisplayBtnColors(Color color)
     {
         for (int i = 0; i < m_displaysAvailable.Length; i++)
